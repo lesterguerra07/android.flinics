f766566971a06455f6f5086601eb373550495bb8
Lester Guerra <25490869+lesterguerra07@users.noreply.github.com>
2019-07-07 13:06:55 -0600

    Initial commit

---
+++ app/src/main/java/com/flinics/history/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.flinics.history.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.flinics.history.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{
+            R.string.tab_general_info
+            , R.string.tab_reason_consulation
+            , R.string.tab_history_current_disease
+            , R.string.tab_pathological_history
+            , R.string.tab_systems_review
+            , R.string.tab_social_profile
+            , R.string.tab_anthropometric_measures
+            , R.string.tab_vital_signs
+            , R.string.tab_general_inspection
+            , R.string.tab_no_pathological_history
+            , R.string.tab_organ_evaluation
+    };
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 11 total pages.
+        return 11;
+    }
+}
