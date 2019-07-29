22d7cc469718f6bb98d12afe0ce7e3fd7da5b6e0
Lester Guerra <25490869+lesterguerra07@users.noreply.github.com>
2019-07-28 18:46:11 -0600

    Se agregó el widget de calendar para ingresar la fecha de nacimiento.

--- app/src/main/java/com/flinics/history/ui/fragment/GeneralInfoFragment.java
+++ app/src/main/java/com/flinics/history/ui/fragment/GeneralInfoFragment.java
@@ -1,16 +1,24 @@
 package com.flinics.history.ui.fragment;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
 import com.flinics.history.R;
+
+import java.util.Calendar;
 
 
 /**
@@ -26,6 +34,18 @@ public class GeneralInfoFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    //Calendar to get date
+    public final Calendar calendar = Calendar.getInstance();
+    //date variables
+    final int month = calendar.get(Calendar.MONTH);
+    final int day = calendar.get(Calendar.DAY_OF_MONTH);
+    final int year = calendar.get(Calendar.YEAR);
+    private static final String _zero = "0";
+    private static final String _slash = "/";
+
+    Button btnDate;
+    EditText etDate;
 
     // TODO: Rename and change types of parameters
     private String _general_info_data;
@@ -68,7 +88,32 @@ public class GeneralInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_general_info, container, false);
+        View view =  inflater.inflate(R.layout.fragment_general_info, container, false);
+        btnDate = (Button) view.findViewById(R.id.birthdate_Button);
+        etDate = (EditText) view.findViewById(R.id.birthdate_EText);
+        Log.d("FragmentTest", btnDate != null ? "IS NOT NULL" : "IS NULL");
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getDate(getContext());
+            }
+        });
+        return view;
+    }
+
+    public void getDate(Context context){
+        DatePickerDialog pickDate = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                final int current_month = month + 1;
+                String format_day = (dayOfMonth < 10)? _zero + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+                String format_month = (current_month < 10)? _zero + String.valueOf(current_month):String.valueOf(current_month);
+                etDate.setText(format_day + _slash + format_month + _slash + year);
+                btnDate.setText(format_day + _slash + format_month + _slash + year);
+            }
+        },year, month, day);
+        //show datepicker widget
+        pickDate.show();
     }
 
     // TODO: Rename method, update argument and hook method into UI event
